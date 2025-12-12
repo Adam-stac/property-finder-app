66293d381a5538eaa7b3efe8f990128a47194985
Adam-stac <adammohamed_1@outlook.com>
2025-12-12 01:26:28 +0000

    feat: make PropertyCard items link to individual property detail pages

--- src/components/PropertyCard/PropertyCard.jsx
+++ src/components/PropertyCard/PropertyCard.jsx
@@ -1,30 +1,34 @@
 import './PropertyCard.css';
+import { Link } from "react-router-dom";
+
 
 const PropertyCard = ({ property }) => {
   const description = property.shortDescription;
   
   return (
-    <div className="property-card">
-      <img 
-        src={property.picture} 
-        alt={`Property in ${property.location}`}
-        className="property-image"
-      />
+    <Link to={`/property/${property.id}`} className="property-link">
+      <div className="property-card">
+        <img 
+          src={property.picture} 
+          alt={`Property in ${property.location}`}
+          className="property-image"
+        />
 
-      <div className="property-price">
-        £{property.price.toLocaleString()}
-      </div>
+        <div className="property-price">
+          £{property.price.toLocaleString()}
+        </div>
 
-      <p className="property-description">
-        {description}
-      </p>
-      
-      <div className="property-details">
-        <span className="property-type">{property.type}</span>
-        <span className="property-bedrooms">{property.bedrooms} bedroom{property.bedrooms !== 1 ? 's' : ''}</span>
-        <span className="property-location">{property.location}</span>
+        <p className="property-description">
+          {description}
+        </p>
+        
+        <div className="property-details">
+          <span className="property-type">{property.type}</span>
+          <span className="property-bedrooms">{property.bedrooms} bedroom{property.bedrooms !== 1 ? 's' : ''}</span>
+          <span className="property-location">{property.location}</span>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
